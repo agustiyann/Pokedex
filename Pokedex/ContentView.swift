da4e0644ecedc1714a6e3d0cb9ecbb8f02fe5450
Agus Tiyan <agustiyaninfo@gmail.com>
2021-03-27 13:51:38 +0700

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Agus Tiyansyah Syam on 27/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
