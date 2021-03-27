acfc893fa63b4455c9476be4f2528b1fede7fcc6
Agus Tiyan <agustiyaninfo@gmail.com>
2021-03-27 14:21:03 +0700

    Add SwiftLint

--- Pokedex/App/SceneDelegate.swift
+++ Pokedex/App/SceneDelegate.swift
@@ -12,21 +12,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
 
-
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+             options connectionOptions: UIScene.ConnectionOptions
+  ) {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    // This delegate does not imply the connecting scene or session are new
+    // (see `application:configurationForConnectingSceneSession` instead).
 
     // Create the SwiftUI view that provides the window contents.
     let contentView = ContentView()
 
     // Use a UIHostingController as window root view controller.
     if let windowScene = scene as? UIWindowScene {
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UIHostingController(rootView: contentView)
-        self.window = window
-        window.makeKeyAndVisible()
+      let window = UIWindow(windowScene: windowScene)
+      window.rootViewController = UIHostingController(rootView: contentView)
+      self.window = window
+      window.makeKeyAndVisible()
     }
   }
 
@@ -34,7 +36,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // Called as the scene is being released by the system.
     // This occurs shortly after the scene enters the background, or when its session is discarded.
     // Release any resources associated with this scene that can be re-created the next time the scene connects.
-    // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    // The scene may re-connect later, as its session was not necessarily discarded
+    // (see `application:didDiscardSceneSessions` instead).
   }
 
   func sceneDidBecomeActive(_ scene: UIScene) {
@@ -58,6 +61,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // to restore the scene back to its current state.
   }
 
-
 }
-
