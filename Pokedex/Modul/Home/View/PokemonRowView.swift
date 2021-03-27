264c4a7c4d6f02697ca052aaf53ef833c34b5124
Agus Tiyan <agustiyaninfo@gmail.com>
2021-03-27 18:43:36 +0700

    Update: get average color from image

--- Pokedex/Modul/Home/View/PokemonRowView.swift
+++ Pokedex/Modul/Home/View/PokemonRowView.swift
@@ -13,18 +13,45 @@ struct PokemonRowView: View {
   var pokemon: PokemonModel
   var index: Int
 
+  @State private var backgroundColor: Color = .clear
+
   var body: some View {
     VStack {
       imageCategory
       content
     }
+    .frame(width: UIScreen.main.bounds.width - 32, height: 250)
+    .background(backgroundColor)
+    .cornerRadius(30)
+    .onAppear {
+      self.setAverageColor()
+    }
+
   }
+
+  private func setImageUrl(for indexImage: Int) -> String {
+    let imagePathUrl = "https://pokeres.bastionbot.org/images/pokemon/"
+    return "\(imagePathUrl)\(indexImage).png"
+  }
+
+  private func setAverageColor() {
+    var image: UIImage?
+    let urlString = setImageUrl(for: self.index)
+
+    let url = NSURL(string: urlString)! as URL
+    if let imageData: NSData = NSData(contentsOf: url) {
+        image = UIImage(data: imageData as Data)
+    }
+    let uiColor = image?.averageColor ?? .clear
+    backgroundColor = Color(uiColor)
+  }
+
 }
 
 extension PokemonRowView {
 
   var imageCategory: some View {
-    WebImage(url: URL(string: "https://pokeres.bastionbot.org/images/pokemon/\(self.index).png"))
+    WebImage(url: URL(string: self.setImageUrl(for: self.index)))
       .resizable()
       .indicator(.activity)
       .transition(.fade(duration: 0.5))
@@ -38,6 +65,7 @@ extension PokemonRowView {
     VStack(alignment: .leading, spacing: 10) {
       Text(pokemon.name)
         .font(.title)
+        .foregroundColor(.white)
         .bold()
     }.padding(
       EdgeInsets(
