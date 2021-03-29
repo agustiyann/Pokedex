6830956c67ec150aa062148498fe93a5bf5f7fe1
Agus Tiyan <agustiyaninfo@gmail.com>
2021-03-29 19:58:35 +0700

    Update about UI

--- Pokedex/Modul/About/View/AboutView.swift
+++ Pokedex/Modul/About/View/AboutView.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct AboutView: View {
-    var body: some View {
-      Text("About")
+
+  @AppStorage("name") var name = "Agus Tiyansyah Syam"
+  @AppStorage("about") var about = "IT Student - Aspiring iOS Developer"
+  @AppStorage("image") var image = UIImage(imageLiteralResourceName: "me").jpegData(compressionQuality: 1)!
+
+  @State var linkEdit = false
+
+  var body: some View {
+    VStack(alignment: .center) {
+      profileImage
+      Text(name)
+        .font(.title3)
+        .fontWeight(.bold)
+        .padding(.top)
+      Text(about)
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .multilineTextAlignment(.center)
+        .padding(.horizontal)
+
+      Button(action: { linkEdit = true }, label: {
+        Text("Edit")
+      })
+      .padding()
+      .buttonStyle(FilledButton())
+
+      Spacer()
     }
+    .background(
+      NavigationLink(
+        destination: EditAboutView(), isActive: $linkEdit) {}
+    )
+  }
+}
+
+extension AboutView {
+
+  var profileImage: some View {
+    Image(uiImage: UIImage(data: self.image) ?? UIImage(imageLiteralResourceName: "me"))
+      .resizable()
+      .scaledToFill()
+      .frame(width: 150, height: 150, alignment: .center)
+      .cornerRadius(100)
+      .padding(.top)
+      .shadow(color: .black, radius: 5, x: 0.0, y: 0.0)
+  }
+
 }
 
 struct AboutView_Previews: PreviewProvider {
-    static var previews: some View {
-        AboutView()
+  static var previews: some View {
+    NavigationView {
+      AboutView()
     }
+  }
 }
---
+++ Pokedex/Modul/About/View/EditAboutView.swift
@@ -0,0 +1,20 @@
+//
+//  EditAboutView.swift
+//  Pokedex
+//
+//  Created by Agus Tiyansyah Syam on 29/03/21.
+//
+
+import SwiftUI
+
+struct EditAboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditAboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditAboutView()
+    }
+}
