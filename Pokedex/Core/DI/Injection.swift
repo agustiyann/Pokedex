61308da0e2d6b08a89bbe7d58329cc19b9647081
Agus Tiyan <agustiyaninfo@gmail.com>
2021-04-07 15:22:33 +0700

    Delete unused codes

--- Pokedex/Core/DI/Injection.swift
+++ Pokedex/Core/DI/Injection.swift
@@ -80,31 +80,4 @@ final class Injection: NSObject {
     return Interactor(repository: repository) as! U
   }
 
-  private func provideRepository() -> PokemonRepositoryProtocol {
-    let realm = try? Realm()
-    let locale: LocalDataSource = LocalDataSource.sharedInstance(realm)
-    let remote: RemoteDataSource = RemoteDataSource.sharedInstance
-    return PokemonRepository.sharedInstance(locale, remote)
-  }
-
-  func provideHome() -> HomeUseCase {
-    let repository = provideRepository()
-    return HomeInteractor(repository: repository)
-  }
-
-  func provideInfo(pokemon: PokemonModel) -> InfoUseCase {
-    let repository = provideRepository()
-    return InfoInteractor(repository: repository, pokemon: pokemon)
-  }
-
-//  func provideFavorite() -> FavoriteUseCase {
-//    let repository = provideRepository()
-//    return FavoriteInteractor(repository: repository)
-//  }
-
-  func provideSearch() -> SearchUseCase {
-    let repository = provideRepository()
-    return SearchInteractor(repository: repository)
-  }
-
 }
